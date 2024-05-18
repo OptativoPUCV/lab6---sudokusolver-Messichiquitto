de9efe7a958454b6ab4094647931ac2a56430287
Your Name <you@example.com>
2024-05-18 18:17:33 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -45,19 +45,35 @@ void print_node(Node* n){
 
 int is_valid(Node* n){
    int i,j;
+   
    for(i=0;i<9;i++){
-      int* arr=(int*) calloc(10,sizeof(int));
-      for(j=0;j<9;j++){
-         if(n->sudo[i][j]==0) 
-            continue;
-         if(arr[n->sudo[i][j]]) 
-            return 0;
-         arr[n->sudo[i][j]]=1;
+      int fil[10] = {0};
+      for(j=0; j<9; j++){
+         if(n->sudo[i][j]!=0){
+            if(fil[n->sudo[i][j]]==0)
+               fil[n->sudo[i][j]]=1;
+            else
+               return 0;
+         }
       }
-      free(arr);
    }
+
+   for(i=0;i<9;i++){
+      int col[10] = {0};
+      for(j=0; j<9; j++){
+         if(n->sudo[j][i]!=0){
+            if(col[n->sudo[j][i]]==0){
+               col[n->sudo[j][i]]=1;
+            }
+            else
+               return 0;
+         }
+      }
+   }
+
+   
    for(i = 0; i < 3; i++){
-      int* arr=(int*) calloc(10,sizeof(int));
+      int arr[10] = {0};
       for(j = 0; j < 9; j++){
          int h = 3*(i/3) + (j/3);
          int v = 3*(i%3) + (j%3);
@@ -66,28 +82,29 @@ int is_valid(Node* n){
          if(matriz != 0){
             if(arr[matriz] == 0)
                arr[matriz] = 1;
+            else
+               return 0;
          }
-         else
-            return 0;
       }
    }
-    return 1;
+   return 1;
 }
-
 
 List* get_adj_nodes(Node* n){
     List* list=createList();
     int i,j;
     for(i=0;i<9;i++){
        for(j=0;j<9;j++){
-          for(int k = 0; k < 9; k++){
-             if(n->sudo[i][j] == 0){
-                Node* adj = copy(n);
-                adj->sudo[i][j] = k+1;
-                if(is_valid(adj)){
+          if(n->sudo[i][j]==0){
+             for(int k = 1; k < 10; k++){
+                n->sudo[i][j] = k;
+                if(is_valid(n)){
+                   Node* adj = copy(n);
                    pushBack(list, adj);
                 }
              }
+             n->sudo[i][j] = 0;
+             return list;
           }
        }
     }
